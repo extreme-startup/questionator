86f70f64646789e7a667340a9b30c19d10e38c21
Andriy Ivashchenko <andr.ivas12@gmail.com>
2019-03-10 20:20:10 +0200

    STORY_14: Initialize and set up a blank database for local development

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomerModule } from './modules/Customer/customer.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(), CustomerModule],
   controllers: [AppController],
   providers: [AppService],
 })
