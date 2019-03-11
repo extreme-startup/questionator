f024dd96996e9c1955caafff0243d64325d7588c
Viktor Melnyk <viktor.melnyk@meta.ua>
2019-03-11 12:00:10 +0200

    feature: added swagger to server (#12)
    Issue: https://github.com/extreme-startup/questionator/issues/6

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from './config';
 import { AppModule } from './app.module';
+import { description, version } from '../package.json';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
+
+  const options = new DocumentBuilder()
+    .setTitle(description)
+    .setVersion(version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(configService.port());
 }
 bootstrap();
