4d39fafa23bca0d037a36f916344f4c6ce24647b
Mykola Dmytryshak <Mykola_Dmytryshak@epam.com>
2019-03-16 09:11:56 +0200

    feat(#15) - auth session PoC

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Request,
+  Res,
+  Post,
+  Body,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +13,19 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('hello')
-  getHello(): string {
+  getHello(@Request() req): string {
     return this.appService.getHello();
+  }
+
+  @Get('sessions')
+  getSessions(@Request() req): string {
+    return req.sessionStore;
+  }
+
+  @Post('auth')
+  auth(@Request() req, @Body() user) {
+    if (!req.session.user) {
+      req.session.user = user.username;
+    }
   }
 }
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,44 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { ExpressSessionMiddleware } from '@nest-middlewares/express-session';
+import * as MySQLStore from 'express-mysql-session';
 import { ConfigModule } from './config/config.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as typeORM from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerModule } from './modules/Customer/customer.module';
+import { ConfigService } from './config/config.service';
 
 @Module({
   imports: [ConfigModule, TypeOrmModule.forRoot(), CustomerModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly configService: ConfigService) {}
+
+  configure(consumer: MiddlewareConsumer) {
+    // const options = {
+    //   host: 'localhost',
+    //   port: 3306,
+    //   user: 'root',
+    //   password: 'password',
+    //   database: 'questionator',
+    // };
+
+    // const sessionStore = MySQLStore(options);
+    // const sessionStore = MySQLStore({}, typeORM.getConnection());
+
+    ExpressSessionMiddleware.configure({
+      secret: this.configService.secretKey(),
+      // store: sessionStore,
+      resave: false,
+      saveUninitialized: false,
+    });
+
+    consumer.apply(ExpressSessionMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
