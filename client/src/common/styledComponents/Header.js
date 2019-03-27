6d9a85c5361f98ca18215078656dba7e425bc6d0
Pavel Mashchenko <pmashchenko@gmail.com>
2019-03-27 23:52:52 +0200

    change reset to normalize and code review fixes

---
+++ client/src/common/styledComponents/Header.js
@@ -0,0 +1,12 @@
+import styled from 'vue-styled-components';
+
+export const TitleH1 = styled.h1`
+  font-size: 36px;
+  font-weight: 900;
+  color: var(--text-color);
+`;
+
+export const TitleH3 = styled(TitleH1)`
+  font-size: 18px;
+  font-weight: bold;
+`;
