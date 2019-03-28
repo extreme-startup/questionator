b0872a2476ce0afe7c503277d25939d7ac22a85f
Oleksandr Krupko <Oleksandr_Krupko@epam.com>
2019-03-28 16:02:13 +0200

    feat(22): Add CRUD endpoints for contest

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerModule } from './modules/Customer/customer.module';
+import { ContestModule } from './modules/Contest/contest.module';
 
 @Module({
-  imports: [ConfigModule, TypeOrmModule.forRoot(), CustomerModule],
+  imports: [ConfigModule, TypeOrmModule.forRoot(), CustomerModule, ContestModule],
   controllers: [AppController],
   providers: [AppService],
 })
