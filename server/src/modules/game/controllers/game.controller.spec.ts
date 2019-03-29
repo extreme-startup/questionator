c373556a93f661010dbadd8bcd20f5a663b68cdf
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2019-03-29 17:25:01 +0200

    Fix specs

--- server/src/modules/game/controllers/game.controller.spec.ts
+++ server/src/modules/game/controllers/game.controller.spec.ts
@@ -2,14 +2,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { GameController } from './game.controller';
 import { GameService } from '../services/game.service';
 import { ConfigService } from '../../../config';
+import { GameRepository } from '../repository/game.repository';
 
 describe('Game Controller', () => {
   let controller: GameController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [GameController, GameService],
-      providers: [GameService, ConfigService],
+      controllers: [GameController],
+      providers: [GameService, ConfigService, GameRepository],
     }).compile();
 
     controller = module.get<GameController>(GameController);
--- server/src/modules/game/controllers/players.controller.spec.ts
+++ server/src/modules/game/controllers/players.controller.spec.ts
@@ -1,13 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PlayersController } from './players.controller';
 import { PlayersService } from '../services/players.service';
+import { PlayersRepository } from '../repository/players.repository';
 
 describe('Players Controller', () => {
   let controller: PlayersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [PlayersController, PlayersService],
+      controllers: [PlayersController],
+      providers: [PlayersService, PlayersRepository],
     }).compile();
 
     controller = module.get<PlayersController>(PlayersController);
