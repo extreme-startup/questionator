9b54f38394eeeb1a5a3886679caaa3c725d744a6
Mykyta.Kovalevskyi <kovnick1@gmail.com>
2019-04-03 03:05:30 +0300

    doc(#0): Add parameters and responses to swagger docs.

--- server/src/entity/Contest.ts
+++ server/src/entity/Contest.ts
@@ -1,24 +1,31 @@
 import { Entity, Column, PrimaryColumn } from 'typeorm';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Contest {
+  @ApiModelProperty()
   @PrimaryColumn('integer', {
     generated: true,
   })
   id: number;
 
+  @ApiModelProperty()
   @Column('varchar')
   name: string;
 
+  @ApiModelProperty()
   @Column('text')
   description: string;
 
+  @ApiModelProperty()
   @Column('varchar')
   category: string;
 
+  @ApiModelProperty()
   @Column('boolean')
   isDeleted: boolean;
 
+  @ApiModelProperty()
   @Column('integer')
   ownerId: number;
 }
