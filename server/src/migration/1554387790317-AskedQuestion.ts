ccd75aa21a118c76534ee1cd61e1c230353a1e3b
Anatolii Karelov <anatolii.karelov@kingfisher.com>
2019-04-04 17:11:25 +0100

    Fix type of the `ID` field for the class; add `is_correct` field

---
+++ server/src/migration/1554387790317-AskedQuestion.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class AskedQuestion1554387790317 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'asked_question',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            generationStrategy: 'uuid',
+            isGenerated: true,
+            isPrimary: true,
+          },
+          {
+            name: 'contest_contender_id',
+            type: 'integer',
+          },
+          { name: 'asked_on', type: 'datetime' },
+          { name: 'answered_on', type: 'datetime' },
+          { name: 'question_id', type: 'varchar' },
+          { name: 'generated_answer', type: 'varchar' },
+          { name: 'generated_question', type: 'varchar' },
+          { name: 'answer', type: 'varchar' },
+          { name: 'score', type: 'numeric' },
+          { name: 'is_correct', type: 'boolean', default: false },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable('contest');
+  }
+}
