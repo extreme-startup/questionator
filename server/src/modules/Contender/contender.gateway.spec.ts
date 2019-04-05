e10e8795fad371634b74c8b3f4d9d53d98ec5db5
Viktor Melnyk <Viktor_Melnyk@epam.com>
2019-04-05 16:46:30 +0300

    tests

---
+++ server/src/modules/Contender/contender.gateway.spec.ts
@@ -0,0 +1,96 @@
+import { ContenderGateway } from './contender.gateway';
+import { Contender } from './dto/Contender';
+
+describe('ContenderGateway', () => {
+  let sut: ContenderGateway,
+    clientMock: any;
+
+  beforeEach(() => {
+    clientMock = {
+      emit: jasmine.createSpy('emit')
+    };
+
+    sut = new ContenderGateway();
+  });
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined();
+  });
+
+  describe('handleDisconnect', () => {
+    it('should remove contender by client', () => {
+      spyOn(sut, 'removeContenderByClient');
+      sut.handleDisconnect('client' as any);
+
+      expect(sut.removeContenderByClient)
+        .toHaveBeenCalledWith('client')
+    });
+  });
+
+  describe('onJoin', () => {
+    beforeEach(() => {
+      spyOn(sut, 'addContender');
+
+      sut.onJoin(clientMock, {login: 'login', sessionURL: ''})
+    });
+
+    it('should add contender', () => {
+      expect(sut.addContender)
+        .toHaveBeenCalledWith(new Contender('login', clientMock))
+    });
+
+    it('should respnd to client', () => {
+      expect(clientMock.emit)
+        .toHaveBeenCalled();
+    });
+  });
+
+  describe('getAnswer', () => {
+    beforeEach(() => {
+      sut.getContenderByEmail = jasmine.createSpy('getContenderByEmail')
+        .and.returnValue({client: clientMock})
+    });
+
+    it('should throw error if no contender', () => {
+      (sut as any).getContenderByEmail
+        .and.returnValue(undefined);
+
+      expect(() => sut.getAnswer('mail', 'auestion'))
+        .toThrow();
+    });
+
+    it('should emit question', () => {
+      sut.getAnswer('mail', 'auestion');
+
+      expect(clientMock.emit)
+        .toHaveBeenCalled();
+    });
+  });
+
+  describe('removeContenderByClient', () => {
+    it('should remove contender', () => {
+      (sut as any).contenders = [
+        {client: clientMock},
+        {client: {}}
+      ];
+      sut.removeContenderByClient(clientMock);
+
+      expect((sut as any).contenders)
+        .toEqual([{client: {}}]);
+    });
+  });
+
+  describe('removeContenderByEmail', () => {
+    it('should remove contender', () => {
+      (sut as any).contenders = [
+        {email: 'email1'},
+        {email: 'email2'}
+      ];
+      sut.removeContenderByEmail('email1');
+
+      expect((sut as any).contenders)
+        .toEqual([{email: 'email2'}]);
+    });
+  });
+
+});
