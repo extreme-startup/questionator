6968b2c42504b8c8654b9cb4226f323aaf05060b
Stanislav Dolgachov <s.dolgachov@gmail.com>
2019-04-08 13:06:27 +0300

    Style app header

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,37 +1,32 @@
 <template>
-  <styled-header>
-    <div>
-      <styled-link hre="/">Questionator</styled-link>
-    </div>
-    <div>
-      <styled-search placeholder="Search" />
-    </div>
-    <div>
-      <styled-profile id="profile" href="/">{{ $store.state.user }}</styled-profile>
-    </div>
-  </styled-header>
+  <v-toolbar color="cyan">
+    <v-toolbar-title>
+      <a href="/" class="white--text">Questionator</a>
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-toollbar-items>
+      <v-container column>
+        <v-layout>
+          <v-flex>
+            <v-text-field dark color="white" placeholder="Search" />
+          </v-flex>
+
+          <v-flex align-self-center>
+            <v-chip
+              ><a class="subheading" id="profile" href="/">{{ $store.state.user }}</a></v-chip
+            >
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-toollbar-items>
+  </v-toolbar>
 </template>
 
 <script>
-import styled from 'vue-styled-components';
-import { Header, Search } from '../common/styledComponents';
-
-const Link = styled.a`
-  font-size: 20px;
-  font-weight: 900;
-`;
-
-const ProfileLink = styled.a`
-  font-size: 18px;
-`;
-
 export default {
   name: 'Header',
-  components: {
-    'styled-header': Header,
-    'styled-link': Link,
-    'styled-profile': ProfileLink,
-    'styled-search': Search,
-  },
+  components: {},
 };
 </script>
