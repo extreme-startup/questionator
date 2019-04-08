c1bd84e23186c52eeeb7be376f038685a993b451
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2019-04-08 16:48:13 +0300

    Fix reference

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -17,7 +17,7 @@ import { ClearCookieMiddleware } from './midelwares/clearCookie.middleware';
 import { ContestModule } from './modules/Contest/contest.module';
 import { QuestionModule } from './modules/Question/question.module';
 import { ContenderModule } from './modules/Contender/contender.module';
-import { GameModule } from './modules/game/game.module';
+import { GameModule } from './modules/Game/game.module';
 
 @Module({
   imports: [
