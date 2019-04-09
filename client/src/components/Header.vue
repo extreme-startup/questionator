e19e028f705b15c3770c55a17a232927db920a4c
Stanislav Dolgachov <s.dolgachov@gmail.com>
2019-04-09 10:20:12 +0300

    Update markup for sessions and training

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,26 +1,24 @@
 <template>
   <v-toolbar color="cyan">
     <v-toolbar-title>
-      <a href="/" class="white--text">Questionator</a>
+      <h1><a href="/" class="white--text">Questionator</a></h1>
     </v-toolbar-title>
 
     <v-spacer></v-spacer>
 
-    <v-toollbar-items>
-      <v-container column>
-        <v-layout>
-          <v-flex>
-            <v-text-field dark color="white" placeholder="Search" />
-          </v-flex>
+    <v-toolbar-items>
+      <v-layout>
+        <v-flex>
+          <v-text-field dark color="white" placeholder="Search" />
+        </v-flex>
 
-          <v-flex align-self-center>
-            <v-chip
-              ><a class="subheading" id="profile" href="/">{{ $store.state.user }}</a></v-chip
-            >
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-toollbar-items>
+        <v-flex align-self-center>
+          <v-chip
+            ><a class="subheading" id="profile" href="/">{{ $store.state.user }}</a></v-chip
+          >
+        </v-flex>
+      </v-layout>
+    </v-toolbar-items>
   </v-toolbar>
 </template>
 
