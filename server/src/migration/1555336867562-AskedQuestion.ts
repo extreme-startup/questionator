15a046e087620b50414bac160c39b43ce81a294c
Nazar Rudenko <nazar_rudenko@epam.com>
2019-04-15 17:59:19 +0300

    feat(#33)-set-up-static-questions
    proceeded Nikita's feedback

--- server/src/migration/1555336867562-AskedQuestion.ts
+++ server/src/migration/1555336867562-AskedQuestion.ts
@@ -4,10 +4,12 @@ export class AskedQuestion1555336867562 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query(`ALTER TABLE "asked_question" ALTER COLUMN "generated_question" RENAME TO "question"`);
+        await queryRunner.query(`ALTER TABLE "asked_question" ALTER COLUMN "generated_answer" RENAME TO "answer"`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`ALTER TABLE "asked_question" ALTER COLUMN "generated_answer" RENAME TO "answer"`);
+        await queryRunner.query(`ALTER TABLE "asked_question" ALTER COLUMN "question" RENAME TO "generated_question"`);
+        await queryRunner.query(`ALTER TABLE "asked_question" ALTER COLUMN "answer" RENAME TO "generated_answer"`);
     }
 
 }
