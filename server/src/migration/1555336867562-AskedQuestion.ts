b0b248b323fa89085a6207f86de8fe8ead8be9bf
Nazar Rudenko <nazar_rudenko@epam.com>
2019-04-16 11:46:08 +0300

    feat(#33)-set-up-static-questions
    fixed migration

--- server/src/migration/1555336867562-AskedQuestion.ts
+++ server/src/migration/1555336867562-AskedQuestion.ts
@@ -3,13 +3,13 @@ import {MigrationInterface, QueryRunner} from 'typeorm';
 export class AskedQuestion1555336867562 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`ALTER TABLE "asked_question" ALTER COLUMN "generated_question" RENAME TO "question"`);
-        await queryRunner.query(`ALTER TABLE "asked_question" ALTER COLUMN "generated_answer" RENAME TO "answer"`);
+        await queryRunner.query("ALTER TABLE `asked_question` CHANGE `generated_question` `question` varchar(255) NOT NULL");
+        await queryRunner.query("ALTER TABLE `asked_question` DROP COLUMN `generated_answer`");
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`ALTER TABLE "asked_question" ALTER COLUMN "question" RENAME TO "generated_question"`);
-        await queryRunner.query(`ALTER TABLE "asked_question" ALTER COLUMN "answer" RENAME TO "generated_answer"`);
+        await queryRunner.query("ALTER TABLE `asked_question` CHANGE `question` `generated_question` varchar(255) NOT NULL");
+        await queryRunner.query("ALTER TABLE `asked_question` ADD `generated_answer` varchar(255) NOT NULL");
     }
 
 }
