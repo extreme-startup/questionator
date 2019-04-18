73317141adda0ec48538812f1ec148337669b36e
Viktor Melnyk <Viktor_Melnyk@epam.com>
2019-04-18 12:18:15 +0300

    ...

--- server/src/modules/Contender/contender.gateway.ts
+++ server/src/modules/Contender/contender.gateway.ts
@@ -3,8 +3,8 @@ import { Client, Server } from 'socket.io';
 import { JoinResponseDto } from './dto/joinResponse.dto';
 import { Answer } from './dto/answer.dto';
 import { JoinRequestDto } from './dto/joinRequest.dto';
-import { Observable, Subject } from 'rxjs';
-import { filter, first, map } from 'rxjs/internal/operators';
+import { Observable, of, Subject, throwError } from 'rxjs';
+import { filter, first, switchMap } from 'rxjs/internal/operators';
 import { Contender } from './dto/Contender';
 
 const JOIN_MESSAGE_NAME = 'join';
@@ -34,6 +34,7 @@ export class ContenderGateway implements OnGatewayConnection {
 
     this.addContender(new Contender(joinRequest.login, client));
     client.emit(JOIN_MESSAGE_NAME, response);
+    this.getAnswer(joinRequest.login, '123').subscribe();
   }
 
   @SubscribeMessage(ANSWER_MESSAGE_NAME)
@@ -43,16 +44,21 @@ export class ContenderGateway implements OnGatewayConnection {
 
   getAnswer(email: string, question: string): Observable<string> {
     const contender = this.getContenderByEmail(email);
+    const hash = this.getHash(question);
+
     if (!contender) {
       return Observable.throw('No such contender');
     }
-    contender.client.emit(QUESTION_MESSAGE_NAME, question);
+    contender.client.emit(QUESTION_MESSAGE_NAME, {question, hash});
 
     return this.answerSubj
       .pipe(
-        filter(({login}: Answer) => login === email),
+        filter(({login, hash: answerHash}: Answer) => login === email && answerHash === hash),
         first(),
-        map(({answer}) => answer),
+        switchMap(({answer, success}: Answer) => success
+          ? of(answer)
+          : throwError('Could not get answer')
+        )
       );
   }
 
@@ -73,5 +79,14 @@ export class ContenderGateway implements OnGatewayConnection {
   removeContenderByEmail(email: string): void {
     this.contenders = this.contenders
       .filter(({email: cEmail}) => cEmail !== email);
+  }
+
+  getHash(question: string) {
+    let s = question + Math.random()+ new Date();
+    return s
+      .split("")
+      .reduce((prevHash, currVal) =>
+        (((prevHash << 5) - prevHash) + currVal.charCodeAt(0))|0, 0)
+      .toString();
   }
 }
