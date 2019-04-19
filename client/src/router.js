2f4fb57d8aa7dd6c734b71d36262af43a13d6e2b
Mykola Dmytryshak <Mykola_Dmytryshak@epam.com>
2019-04-19 17:10:40 +0300

    remove about.vue

--- client/src/router.js
+++ client/src/router.js
@@ -19,14 +19,6 @@ const router = new Router({
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
-    {
       path: '/login',
       name: 'login',
       component: Login,
