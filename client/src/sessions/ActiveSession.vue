2875a0f0c22f9245e634f4dd1f74a23d44d638bc
Mykola Dmytryshak <nickothin.yangol@gmail.com>
2019-04-20 11:29:15 +0300

    Leader board (#117)
    * leader board websocket connection
    
    * add chart
    
    * merge
    
    * add training session page
    
    * remove websocket
    
    * remove websocket deps
    
    * satisfy lint
    
    * remove ref to HelloWorld.vue
    
    * add leader board to session page
    
    * restore script
    
    * remove about.vue
    
    * use arrow function

--- client/src/sessions/ActiveSession.vue
+++ client/src/sessions/ActiveSession.vue
@@ -1,89 +1,69 @@
 <template>
-  <v-dialog v-model="isOpen" full-width persistent>
-    <v-card>
-      <v-container>
-        <v-layout row justify-space-between>
-          <v-flex md5 class="title font-weight-black">
-            <v-card-text>Questionaire</v-card-text>
-          </v-flex>
-          <v-flex md2>
-            <v-text-field placeholder="Search" />
-          </v-flex>
-          <v-flex md2 subheading align-self-end>
-            <v-card-text>Profile</v-card-text>
-          </v-flex>
-        </v-layout>
-        <v-layout row>
-          <v-flex md2>
-            <v-btn @click="close">Back to workshop</v-btn>
-          </v-flex>
-        </v-layout>
-        <v-layout row>
-          <v-flex md2>
-            <v-card-text class="headline font-weight-black">Session#{{ session.id }}</v-card-text>
-          </v-flex>
-          <v-flex md8>
-            <v-card-text class="subheading">{{ session.status }}</v-card-text>
-          </v-flex>
-          <v-flex md3 align="right">
-            <v-card-text class="subheading">Trainer: {{ session.trainer.email }}</v-card-text>
-          </v-flex>
-        </v-layout>
-        <v-layout row>
-          <v-flex md6>
-            <v-text-field readonly value="session.link" id="activeSessionLinkId" type="text" />
-          </v-flex>
-          <v-flex md2>
-            <v-btn v-on:click="copyGeneratedLink" dark>COPY LINK</v-btn>
-          </v-flex>
-        </v-layout>
-        <v-layout v-if="copyLinkErrorMessage" row>
-          <div error>{{ copyLinkErrorMessage }}</div>
-        </v-layout>
-        <v-layout row>
-          <v-flex xl3>
-            <v-btn dark @click="startActiveSession">START SESSION</v-btn>
-          </v-flex>
-        </v-layout>
-        <v-layout row>
-          <v-flex xl12>
-            <div>
-              <!-- This is the leaderboard placeholder -->
-            </div>
-          </v-flex>
-        </v-layout>
-        <v-layout row>
-          <v-flex md3 subheading font-weight-bold>Members</v-flex>
-        </v-layout>
-        <v-layout row>
-          <v-flex xs12>
-            <contenders-table :sessionId="session.id" />
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-card>
-  </v-dialog>
+  <v-container>
+    <v-layout row>
+      <v-flex md2>
+        <v-btn @click="backToWorkshop">Back to workshop</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex md2>
+        <v-card-text class="headline font-weight-black">Session#{{ session.id }}</v-card-text>
+      </v-flex>
+      <v-flex md8>
+        <v-card-text class="subheading">{{ session.status }}</v-card-text>
+      </v-flex>
+      <v-flex md3 align="right">
+        <v-card-text class="subheading">Trainer: {{ session.trainer.email }}</v-card-text>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex md6>
+        <v-text-field readonly value="session.link" id="activeSessionLinkId" type="text" />
+      </v-flex>
+      <v-flex md2>
+        <v-btn v-on:click="copyGeneratedLink" dark>COPY LINK</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout v-if="copyLinkErrorMessage" row>
+      <div error>{{ copyLinkErrorMessage }}</div>
+    </v-layout>
+    <v-layout row>
+      <v-flex xl3>
+        <v-btn dark @click="startActiveSession">START SESSION</v-btn>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row>
+      <v-flex xl12>
+        <LeaderBoard />
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex md3 subheading font-weight-bold>Members</v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12>
+        <contenders-table :sessionId="session.id" />
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 import ContendersTable from '../contenders/ContendersTable';
+import LeaderBoard from '@/sessions/LeaderBoard.vue';
 
 export default {
   name: 'active-session',
   data() {
     return { copyLinkErrorMessage: '' };
   },
-  props: {
-    isOpen: Boolean,
-    close: Function,
-    session: {
-      id: String,
-      trainerName: String,
-      status: String,
-      link: String,
+  computed: {
+    session: function() {
+      return this.$store.getters['session/activeSession'];
     },
   },
-  components: { ContendersTable },
+  components: { ContendersTable, LeaderBoard },
   methods: {
     copyGeneratedLink() {
       try {
@@ -98,6 +78,11 @@ export default {
     },
     startActiveSession() {
       this.$store.dispatch('session/startSession', this.session.id);
+    },
+    backToWorkshop() {
+      // !TODO! - replace with real training id
+      const trainingId = 1;
+      this.$router.push(`/training/${trainingId}`);
     },
   },
 };
