b41512965676c65dba29b51eb2c6c0f8191c59ca
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2019-04-22 21:35:16 +0300

    Fix questions module

--- server/src/modules/Question/question.controller.ts
+++ server/src/modules/Question/question.controller.ts
@@ -1,46 +1,59 @@
-import { Controller, Get, Param, Post, Body, Put, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { ApiResponse } from '@nestjs/swagger';
 import { QuestionDto } from './dto/question.dto';
-import { AnswerDto } from './dto/answer.dto';
 import { QuestionService } from './question.service';
+import { ResponseDto } from 'src/models/response.dto';
 
 @Controller('questions')
 export class QuestionController {
+  constructor(private readonly questionService: QuestionService) {}
 
-    constructor(private questionService: QuestionService) {}
-
-    @Get()
-    @ApiResponse({ status: 200, type: QuestionDto, isArray: true })
-    async findAll(): Promise<QuestionDto[]> {
-        return await this.questionService.findAll();
-    }
+  @Get()
+  @ApiResponse({ status: 200, type: QuestionDto, isArray: true })
+  public async findAll(): Promise<ResponseDto<QuestionDto[]>> {
+    return await this.questionService.findAll();
+  }
 
   // todo it's a temporary solution and will be implemented in #23/24 stories
-    @Get('random')
-    async getRandom(): Promise<QuestionDto> {
-        return await this.questionService.getRandom();
-    }
+  @Get('random')
+  public async getRandom(): Promise<ResponseDto<QuestionDto>> {
+    return await this.questionService.getRandom();
+  }
 
-    @Get(':id')
-    @ApiResponse({ status: 200, type: QuestionDto })
-    async findOneById(@Param('id') id: string): Promise<QuestionDto> {
-        return await this.questionService.findById(id);
-    }
+  @Get(':id')
+  @ApiResponse({ status: 200, type: QuestionDto })
+  public async findOneById(
+    @Param('id') id: string,
+  ): Promise<ResponseDto<QuestionDto>> {
+    return await this.questionService.findById(id);
+  }
 
-    @Post()
-    @ApiResponse({ status: 200, type: QuestionDto })
-    async create(@Body() question: QuestionDto): Promise<QuestionDto> {
-        return await this.questionService.insert(question);
-    }
+  @Post()
+  @ApiResponse({ status: 200, type: QuestionDto })
+  public async create(
+    @Body() question: QuestionDto,
+  ): Promise<ResponseDto<QuestionDto>> {
+    return await this.questionService.insert(question);
+  }
 
-    @Put(':id')
-    async update(@Body() updatedQuestion: QuestionDto, @Param('id') id: string): Promise<QuestionDto> {
-        const oldQuestion = await this.questionService.findById(id);
-        return await this.questionService.update(oldQuestion, updatedQuestion);
-    }
+  @Put(':id')
+  public async update(
+    @Body() payload: QuestionDto,
+    @Param('id') id: string,
+  ): Promise<ResponseDto<QuestionDto>> {
+    return await this.questionService.update(id, payload);
+  }
 
-    @Delete(':id')
-    async delete(@Param('id') id: string) {
-        return await this.questionService.delete(id);
-    }
+  @Delete(':id')
+  public async delete(@Param('id') id: string) {
+    return await this.questionService.delete(id);
+  }
 }
