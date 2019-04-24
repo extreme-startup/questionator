af31ef1c6f46f4dace4c74c46be2b011ba891e76
Nick <kovnick1@gmail.com>
2019-04-24 11:01:42 +0300

    fix(131): crutch (#147)

--- client/src/api/question.js
+++ client/src/api/question.js
@@ -4,4 +4,4 @@ export const getQuestions = contestId => Vue.axios.get(`/contest/${contestId}/qu
 export const addQuestion = question => Vue.axios.post('/questions', question);
 export const updateQuestion = question => Vue.axios.put(`/questions/${question.id}`, question);
 export const deleteQuestion = question =>
-  Vue.axios.put(`/questions/${question.id}`, { ...question, isDeleted: true });
+  Vue.axios.put(`/questions/${question.id}`, { ...question, deleted: true });
